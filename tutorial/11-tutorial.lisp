a7054d04ead8db4a1eb44feb74972d129fe92094
cms <colin@platform.sh>
2021-02-04 16:13:07 +0000

    typos in comments and docstrings for tutorial 11
    a few minor typos

--- tutorial/11-tutorial.lisp
+++ tutorial/11-tutorial.lisp
@@ -1,26 +1,26 @@
 ;;; Creating code GUIs from scratch is great, but in the real world often design
-;;; is left to artists and code is left to engineers. CLOG is an amazign solution
-;;; for that model of app development. Any existing web page can be come a CLOG
+;;; is left to artists and code is left to engineers. CLOG is an amazing solution
+;;; for that model of app development. Any existing web page can become a CLOG
 ;;; app by simply adding the following two lines to an existing HTML file:
 ;;;
 ;;;      <script src="/js/jquery.min.js" type="text/javascript"></script>
 ;;;      <script src="/js/boot.js" type="text/javascript"></script>
 ;;;
 ;;; The first line adds jquery which CLOG uses to enhance browser compatability.
-;;; The second line add the "boot" file.
+;;; The second line adds the "boot" file.
 ;;;
-;;; For this tutorial we  generated the clog/static-files/tutorial/tut-11.html
+;;; For this tutorial we generated the clog/static-files/tutorial/tut-11.html
 ;;; by using the form generator at https://bootsnipp.com/forms?version=3
 ;;; then used the the template from https://getbootstrap.com/docs/3.3/
 ;;; and added CLOG's boot.js script line. It was neccesary to add an id to the
 ;;; form (id='form1') as the generator did not add one.
 ;;;
 ;;;  - We set a blank on-submit to overide the behavior in the bootstrap
-;;;          buttons to submit the from HTML style.
+;;;          buttons to submit the form HTML style.
 ;;;  - We are going to attach to the "Good Button" an on-click handler
 ;;;          to handle getting the values
 ;;;  - We attach to each control in the on-click handler that we want
-;;;          value for and query them
+;;;          a value for and query them
 ;;;  - We attach an on-click handler that resets the form to the
 ;;;          "Scary Button"
 ;;;
@@ -62,13 +62,13 @@
 	   (on-click-scary (obj)
 	     (declare (ignore obj))
 	     (reset form)))
-    ;; We need to overide the boostrap default to submit the form html style
+    ;; We need to override the boostrap default to submit the form html style
     (set-on-submit form (lambda (obj)(declare (ignore obj))()))
     (set-on-click good-button #'on-click-good)
     (set-on-click scary-button #'on-click-scary))
     (run body)))
 
 (defun start-tutorial ()
-  "Start turtorial."
+  "Start tutorial."
   (initialize #'on-new-window)
   (open-browser :url "http://127.0.0.1:8080/tutorial/tut-11.html"))
