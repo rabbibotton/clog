22451fc49853902fb14bee8b142a12279dc1530d
cms <colin@platform.sh>
2021-02-04 16:59:01 +0000

    another typo in tutorial-13 output "appliction -> application"
    missed this one previously

--- tutorial/13-tutorial.lisp
+++ tutorial/13-tutorial.lisp
@@ -6,7 +6,7 @@
 
 (defun start-tutorial ()
   "Start tutorial."
-  (format t "Tutorial 13 is a how to on building your own clog appliction.~%~
+  (format t "Tutorial 13 is a how to on building your own clog application.~%~
              Copy the directory - ~A~%~
              to your ~~/common-lisp directory or other asdf / quicklisp~%~
              directory. Then follow the directions in the 13-tutorial ~%~
