d8c4d2d011c91b6ce91e5088d5f04f1fcbcdd817
cms <colin@platform.sh>
2021-02-05 11:23:25 +0000

    Edit the instructions in tutorial-13 for accuracy
    It suggests moving the whole tutorial directory into somewhere that
    asdf/quicklisp is going to search, but really all you need is the
    project subdirectory. Also updates the hint to point directly to the
    README.md

--- tutorial/13-tutorial.lisp
+++ tutorial/13-tutorial.lisp
@@ -9,7 +9,7 @@
   (format t "Tutorial 13 is a how to on building your own clog application.~%~
              Copy the directory - ~A~%~
              to your ~~/common-lisp directory or other asdf / quicklisp~%~
-             directory. Then follow the directions in the 13-tutorial ~%~
+             directory. Then follow the directions in the 13-tutorial/README.md ~%~
             directory."
-	  (merge-pathnames "./tutorial/13-tutorial"
+	  (merge-pathnames "./tutorial/13-tutorial/hello-clog/"
 			   (asdf:system-source-directory :clog))))
