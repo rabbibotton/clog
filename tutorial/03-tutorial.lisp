4eba57ec5655c06102b98f048f4807a6ff5ee7c5
David Botton <david@botton.com>
2021-02-06 22:26:03 -0500

    Mention the :one-time key for events

--- tutorial/03-tutorial.lisp
+++ tutorial/03-tutorial.lisp
@@ -23,7 +23,10 @@
 ;;; Running this version of the last tutorial and clicking quickly on the (click me!)
 ;;; will demonstrate an important aspect of CLOG, events can happen in _parallel_.
 ;;; This means that appropriate precautions to thread protect data should be taken
-;;; and that events do not wait for previous event handlers to complete.
+;;; and that events do not wait for previous event handlers to complete. One simple
+;;; way to avoid issues is to use the key :one-time t on the set-on-click or other
+;;; event, this will turn off the event immediately when the user clicks and can then
+;;; set the even again when done handling the event if want to again accept the event.
 (defun start-tutorial ()
   "Start turtorial."
   (initialize #'on-new-window)
