bf0f278cb1c50a81b313396872ca587f947634c3
David Botton <david@botton.com>
2021-03-18 23:12:30 -0400

    mobile it

--- tutorial/26-tutorial.lisp
+++ tutorial/26-tutorial.lisp
@@ -22,19 +22,34 @@
 	 (icon  (create-web-menu-icon menu :on-click (lambda (obj)
 						       (declare (ignore obj))
 						       (setf (hash (location body)) "rung2"))))
+	 (item1 (create-web-menu-item menu :content  "item1"
+					   :on-click (lambda (obj)
+						       (declare (ignore obj))
+						       (setf (hash (location body)) "rung2"))))
+	 (item2 (create-web-menu-item menu :content  "item2"
+					   :on-click (lambda (obj)
+						       (declare (ignore obj))
+						       (setf (hash (location body)) "rung2"))))
+	 (item3 (create-web-menu-item menu :content  "item3"
+					   :on-click (lambda (obj)
+						       (declare (ignore obj))
+						       (setf (hash (location body)) "rung2"))))
 	 (about (create-web-menu-item menu :content  "About"
 					   :on-click (lambda (obj)
 						       (declare (ignore obj))
 						       (setf (hash (location body)) "rung2")))))
     (declare (ignore icon))
+    (full-row-on-mobile item1)
+    (full-row-on-mobile item2)
+    (full-row-on-mobile item3)
     (add-class about "w3-right"))
   ;; rung-1
   (let* ((first-rung (create-web-compositor body :html-id "rung1"))
 	 (image      (create-img first-rung :url-src "/img/windmills.jpg"
 					    :class "w3-sepia"))
 	 (clog-txt   (create-div first-rung :content "CLOG<br><u>The omnificient gui</u><br>
-                                                      desktop<br>web<br>mobile<br>iot"
-					    :class   "w3-text-white w3-xxlarge")))
+                                                      desktop<br>web<br>mobile"
+					    :class   "w3-text-white w3-xlarge")))
     (setf (cursor clog-txt) :pointer)
     (set-on-click clog-txt (lambda (obj)
 			     (declare (ignore obj))
@@ -48,6 +63,7 @@
 	 (image-cell  (create-web-auto-column second-rung))
 	 (image       (create-img image-cell :url-src "/img/flower-clogs.jpg"))
 	 (text-cell   (create-web-auto-column second-rung :class "w3-cell-top")))
+    (hide-on-small-screens image-cell)
     (setf (background-color text-cell) (rgb 199 188 160))
     (setf (box-width image-cell) "40%")
     (setf (box-width image) "100%")
