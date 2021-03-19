6bb42224cefd965584943f04335ac4139c4eaf58
David Botton <david@botton.com>
2021-03-19 13:42:44 -0400

    allow initialize with symbol names in addition to function symbols

--- source/clog-system.lisp
+++ source/clog-system.lisp
@@ -38,7 +38,11 @@ same as the clog directy this overides the relative paths used in them.")
       (if on-new-window
 	  (progn
 	    (setf (connection-data-item body "clog-body") body)
-	    (funcall on-new-window body))
+	    (funcall
+             (if (functionp on-new-window)
+                 on-new-window
+                 (symbol-function on-new-window))
+             body))
 	  (put-br (html-document body) "No route to on-new-window")))))
 
 (defun initialize
