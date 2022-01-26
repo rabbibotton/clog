e78864a6afc1d5c77b74d6ee20923a968ffb0c37
David Botton <david@botton.com>
2022-01-26 15:42:14 -0500

    Update some (c) info

--- source/clog-web.lisp
+++ source/clog-web.lisp
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; CLOG - The Common Lisp Omnificent GUI                                 ;;;;
-;;;; (c) 2020-2021 David Botton                                            ;;;;
+;;;; (c) 2020-2022 David Botton                                            ;;;;
 ;;;; License BSD 3 Clause                                                  ;;;;
 ;;;;                                                                       ;;;;
 ;;;; clog-web.lisp                                                         ;;;;
