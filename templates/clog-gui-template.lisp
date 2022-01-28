1393f13049467a07531702ede94372bacf1c6308
David Botton <david@botton.com>
2022-01-28 16:30:48 -0500

    update clog-gui-template.lisp

--- templates/clog-gui-template.lisp
+++ templates/clog-gui-template.lisp
@@ -2,13 +2,14 @@
 
 (defpackage #:clog-user
   (:use #:cl #:clog #:clog-gui)
-  (:export start-tutorial))
+  (:export start-app))
 
 (in-package :clog-user)
 
 (defun on-file-new (obj)
-  (let* ((win (create-gui-window obj :title "New Window")))
-    ))
+  (let* ((app (connection-data-item obj "builder-app-data"))
+	 (win (create-gui-window obj :title "New Window")))
+    (declare (ignore app win))))
 
 (defun on-help-about (obj)
   (let* ((about (create-gui-window obj
@@ -27,18 +28,25 @@
     (set-on-window-can-size about (lambda (obj)
 				    (declare (ignore obj))()))))
 
+(defclass app-data ()
+  ((data
+    :accessor data)))
+
 (defun on-new-window (body)
-  (setf (title (html-document body)) "New App")
-  (clog-gui-initialize body)
-  (add-class body "w3-teal")  
-  (let* ((menu  (create-gui-menu-bar body))
-	 (tmp   (create-gui-menu-icon menu :on-click #'on-help-about))
-	 (file  (create-gui-menu-drop-down menu :content "File"))
-	 (tmp   (create-gui-menu-item file :content "New Window" :on-click #'on-file-new))
-	 (help  (create-gui-menu-drop-down menu :content "Help"))
-	 (tmp   (create-gui-menu-item help :content "About" :on-click #'on-help-about))
-	 (tmp   (create-gui-menu-full-screen menu)))	 
-  (run body)))
+  (let ((app (make-instance 'app-data)))
+    (setf (connection-data-item body "app-data") app)
+    (setf (title (html-document body)) "New App")
+    (clog-gui-initialize body)
+    (add-class body "w3-teal")  
+    (let* ((menu-bar    (create-gui-menu-bar body))
+	   (icon-item   (create-gui-menu-icon menu-bar :on-click #'on-help-about))
+	   (file-item   (create-gui-menu-drop-down menu-bar :content "File"))
+	   (file-new    (create-gui-menu-item file-item :content "New Window" :on-click #'on-file-new))
+	   (help-item   (create-gui-menu-drop-down menu-bar :content "Help"))
+	   (help-about  (create-gui-menu-item help-item :content "About" :on-click #'on-help-about))
+	   (full-screen (create-gui-menu-full-screen menu-bar)))
+      (declare (ignore icon-item file-new help-about full-screen))
+      (run body))))
 
 (defun start-app ()
   (initialize #'on-new-window)
