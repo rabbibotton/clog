e0331338f7f1f05a9a34adad786596a83d99ecd6
David Botton <david@botton.com>
2022-05-15 14:58:47 -0400

    fix repl url

--- source/clog-docs.lisp
+++ source/clog-docs.lisp
@@ -185,7 +185,7 @@ CLOG-USER> (setf (background-color *body*) \"beige\")
 CLOG-USER> (create-div *body* :content \"Hello World!\")
 ```
 
-The clog-repl URL is http://127.0.0.1/repl *body* will always refer to the
+The clog-repl URL is http://127.0.0.1:8080/repl *body* will always refer to the
 last access of that URL.
 
 To open a browser with the CLOG manual:
