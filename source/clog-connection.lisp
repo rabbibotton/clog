8097a8b344b6aba893bfdbec11dfee9da350c89f
David Botton <david@botton.com>
2022-07-15 09:50:26 -0400

    more secure connection ids

--- source/clog-connection.lisp
+++ source/clog-connection.lisp
@@ -178,7 +178,7 @@ the default answer. (Private)"
              (setf (gethash id *connection-ids*) connection)
              (setf (gethash connection *connections*) id))
             (t
-             (setf id (generate-id))
+             (setf id (+ (get-universal-time) (generate-id)))
              (setf (gethash connection *connections*) id)
              (setf (gethash id *connection-ids*) connection)
              (setf (gethash id *connection-data*)
