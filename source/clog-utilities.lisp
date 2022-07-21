ae6cdb7580de5605fd841647609cf58743de7331
David Botton <david@botton.com>
2022-07-20 23:12:17 -0400

    escape html \n and \r

--- source/clog-utilities.lisp
+++ source/clog-utilities.lisp
@@ -152,8 +152,8 @@ and not for security purposes or html escapes."
         (cond (html
                (setf res (ppcre:regex-replace-all "\\x22" res "&#x22;")) ; "
                (setf res (ppcre:regex-replace-all "\\x27" res "&#x27;")) ; '
-               (setf res (ppcre:regex-replace-all "\\x0A" res "")) ; \n
-               (setf res (ppcre:regex-replace-all "\\x0D" res ""))) ; \r
+               (setf res (ppcre:regex-replace-all "\\x0A" res "&#x0A;")) ; \n
+               (setf res (ppcre:regex-replace-all "\\x0D" res "&#x0D"))) ; \r
               (t
                (setf res (ppcre:regex-replace-all "\\x22" res "\\x22")) ; "
                (setf res (ppcre:regex-replace-all "\\x27" res "\\x27")) ; '
