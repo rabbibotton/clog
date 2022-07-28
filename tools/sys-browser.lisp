15cc6a8c889132fbb635ecfe4eed9e25f501af61
David Botton <david@botton.com>
2022-07-27 20:38:19 -0400

    search to term in sys-browser

--- tools/sys-browser.lisp
+++ tools/sys-browser.lisp
@@ -8,14 +8,14 @@
     (type-box :reader type-box)
 (classes :accessor classes)))
 (defun create-sys-browser (clog-obj &key (hidden nil) (class nil) (html-id nil) (auto-place t))
-  (let ((panel (change-class (clog:create-div clog-obj :content "<select id=\"CLOGB386794791511\" style=\"box-sizing: content-box; position: absolute; left: 5px; top: 10px; width: 190px; height: 20px;\" data-clog-name=\"type-box\"></select><select style=\"box-sizing: content-box; position: absolute; left: 205px; top: 10px; width: 300px; height: 20px; bottom: 335.028px;\" id=\"CLOGB3867947746\" data-clog-name=\"package-box\"></select><select size=\"4\" style=\"box-sizing: content-box; position: absolute; left: 5px; top: 45px; width: 190px; height: 145px;\" class=\"w3-small\" id=\"CLOGB3867947747\" data-clog-name=\"class-box\"></select><textarea name=\"\" cols=\"20\" rows=\"2\" style=\"box-sizing: content-box; position: absolute; left: 205px; top: 45px; height: 139.989px; right: 5px; resize: none; min-width: 0px;\" class=\"&nbsp;w3-small\" id=\"CLOGB3867947748\" data-clog-name=\"doc-box\"></textarea><div class=\"ace_editor ace_hidpi ace-xcode ace-tm\" style=\"border: thin solid black; box-sizing: content-box; position: absolute; inset: 199.991px 5px 30px;\" id=\"CLOGB3867947749\" data-clog-name=\"src-box\"></div><div style=\"box-sizing: content-box; position: absolute; left: 5px; bottom: 5px; right: 5px;\" class=\"w3-tiny w3-border\" id=\"CLOGB3867947750\" data-clog-name=\"status-box\">status</div>"
+  (let ((panel (change-class (clog:create-div clog-obj :content "<select style=\"box-sizing: content-box; position: absolute; left: 5px; top: 10px; width: 190px; height: 20px;\" id=\"CLOGB3867955785\" data-clog-name=\"type-box\"></select><select style=\"box-sizing: content-box; position: absolute; left: 205px; top: 10px; width: 300px; height: 20px; bottom: 335.028px;\" id=\"CLOGB3867955786\" data-clog-name=\"package-box\"></select><select size=\"4\" style=\"box-sizing: content-box; position: absolute; left: 5px; top: 45px; width: 350px; height: 145px;\" class=\"w3-small\" id=\"CLOGB3867955787\" data-clog-name=\"class-box\"></select><textarea name=\"\" cols=\"20\" rows=\"2\" style=\"box-sizing: content-box; position: absolute; inset: 45px 5px 176.051px 365px; height: 139.989px; resize: none; min-width: 0px;\" class=\"&nbsp;w3-small\" id=\"CLOGB3867955788\" data-clog-name=\"doc-box\"></textarea><div class=\"ace_editor ace_hidpi ace-xcode ace-tm\" style=\"border: thin solid black; box-sizing: content-box; position: absolute; inset: 199.991px 5px 30px;\" id=\"CLOGB3867955789\" data-clog-name=\"src-box\"></div><div style=\"box-sizing: content-box; position: absolute; left: 5px; bottom: 5px; right: 5px;\" class=\"w3-tiny w3-border\" id=\"CLOGB3867955790\" data-clog-name=\"status-box\">status</div>"
          :hidden hidden :class class :html-id html-id :auto-place auto-place) 'sys-browser)))
-    (setf (slot-value panel 'status-box) (attach-as-child clog-obj "CLOGB3867947750" :clog-type 'CLOG:CLOG-DIV :new-id t))
-    (setf (slot-value panel 'src-box) (attach-as-child clog-obj "CLOGB3867947749" :clog-type 'CLOG-ACE:CLOG-ACE-ELEMENT :new-id t))
-    (setf (slot-value panel 'doc-box) (attach-as-child clog-obj "CLOGB3867947748" :clog-type 'CLOG:CLOG-TEXT-AREA :new-id t))
-    (setf (slot-value panel 'class-box) (attach-as-child clog-obj "CLOGB3867947747" :clog-type 'CLOG:CLOG-SELECT :new-id t))
-    (setf (slot-value panel 'package-box) (attach-as-child clog-obj "CLOGB3867947746" :clog-type 'CLOG:CLOG-SELECT :new-id t))
-    (setf (slot-value panel 'type-box) (attach-as-child clog-obj "CLOGB386794791511" :clog-type 'CLOG:CLOG-SELECT :new-id t))
+    (setf (slot-value panel 'status-box) (attach-as-child clog-obj "CLOGB3867955790" :clog-type 'CLOG:CLOG-DIV :new-id t))
+    (setf (slot-value panel 'src-box) (attach-as-child clog-obj "CLOGB3867955789" :clog-type 'CLOG-ACE:CLOG-ACE-ELEMENT :new-id t))
+    (setf (slot-value panel 'doc-box) (attach-as-child clog-obj "CLOGB3867955788" :clog-type 'CLOG:CLOG-TEXT-AREA :new-id t))
+    (setf (slot-value panel 'class-box) (attach-as-child clog-obj "CLOGB3867955787" :clog-type 'CLOG:CLOG-SELECT :new-id t))
+    (setf (slot-value panel 'package-box) (attach-as-child clog-obj "CLOGB3867955786" :clog-type 'CLOG:CLOG-SELECT :new-id t))
+    (setf (slot-value panel 'type-box) (attach-as-child clog-obj "CLOGB3867955785" :clog-type 'CLOG:CLOG-SELECT :new-id t))
     (let ((target (type-box panel))) (declare (ignorable target)) (add-select-options target '(ALIEN-TYPE
                              CALLABLE
                              CLASS
@@ -62,8 +62,9 @@
   (setf (text-value (doc-box panel))
     (or (definitions:documentation item)
         "No documentation"))
-  (if fname
-      (setf (text-value (src-box panel)) (read-file fname))
-      (setf (text-value (src-box panel)) "No file information")))
-  ))
+  (cond (fname
+         (setf (text-value (src-box panel)) (read-file fname))
+         (js-execute target (format nil "~A.find('~A')" (clog-ace::js-ace (src-box panel)) (definitions:name item))))
+        (t
+         (setf (text-value (src-box panel)) "No file information"))))))
     panel))
