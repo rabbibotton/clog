cfbebd9d65b6769686fad12b77720427b07382a3
Mariano Montone <marianomontone@gmail.com>
2023-02-03 10:24:52 -0300

    clog-callbacks: register callbacks on the fly
    It's very common to need to have callbacks as lambda expressions,
    so that they have access to the context in which they are being
    created.

--- source/clog-callbacks.lisp
+++ source/clog-callbacks.lisp
@@ -1,5 +1,5 @@
 (mgl-pax:define-package :clog-callbacks
-  (:documentation "The Common List Omnificent GUI - Callbacks")
+    (:documentation "The Common List Omnificent GUI - Callbacks")
   (:use #:cl #:mgl-pax)
   (:export
    :register-callback
@@ -22,7 +22,7 @@
   "Find callback named with NAME."
   (or (gethash name *callbacks*)
       (and error-p
-	   (error "Callback not defined: ~s" name))))
+           (error "Callback not defined: ~s" name))))
 
 (defmacro defcallback (name args &body body)
   "Define a callback function."
@@ -30,10 +30,21 @@
      (defun ,name ,args ,@body)
      (register-callback ,(princ-to-string name) #',name)))
 
-(defun call-callback (name args)
+(defun call-callback (name-or-lambda args)
   "Returns the javascript code for calling a registered callback function.
+NAME-OR-LAMBDA can be either:
+- A symbol. Should name a callback defined via DEFCALLBACK.
+- A function. Probably a lambda. Gets registered as callback and called.
 ARGS is a javascript expression (string) that returns a JSON with the arguments for the callback."
-  (format nil "ws.send('C:~a:' + ~a)" name args))
+  (cond
+    ((symbolp name-or-lambda)
+     (format nil "ws.send('C:~a:' + ~a)" name-or-lambda args))
+    ((functionp name-or-lambda)
+     ;; assign a name and register the callback first
+     (let ((callback-name (gensym "CALLBACK-")))
+       (register-callback (princ-to-string callback-name) name-or-lambda)
+       (format nil "ws.send('C:~a:' + ~a)" callback-name args)))
+    (t (error "Invalid callback: ~s" name-or-lambda))))
 
 (defun handle-callback (name args)
   (let ((callback (find-callback name)))
