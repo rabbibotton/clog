0798c56fda61486886ec0fbd1cadf0cd8bb9865f
David Botton <david@botton.com>
2024-06-30 09:24:13 -0400

    panel autosizes now

--- tools/panel-project-directory.lisp
+++ tools/panel-project-directory.lisp
@@ -1,4 +1,4 @@
-;;;; CLOG Builder generated code - modify original clog file
+;;;; CLOG Builder generated code - modify original .clog file and rerender
 (in-package :clog-tools)
 (defclass project-dir (clog:clog-panel)
           ((select-button :reader select-button)
@@ -9,25 +9,23 @@
   (let ((panel
          (change-class
           (clog:create-div clog-obj :content
-                           "<div style=\"display: flex; flex-direction: column; box-sizing: content-box; position: absolute; inset: 10px 10px 10px 12px; justify-content: normal; align-items: center;\" id=\"CLOGB3871639549\" data-clog-name=\"frame\"><select size=\"4\" style=\"box-sizing: content-box; position: static; width: 400px; height: 200px;\" id=\"CLOGB3871639550\" data-clog-name=\"project-list\"></select><button style=\"box-sizing: content-box; position: static;\" class=\"w3-margin\" id=\"CLOGB3871639551\" data-clog-name=\"select-button\">Select Project Directory</button></div>"
+                           "<div style=\"display: flex; flex-direction: column; box-sizing: content-box; position: absolute; inset: 10px 10px 10px 11px; justify-content: normal; align-items: center;\" id=\"CLOGB3928741015\" data-clog-name=\"frame\"><select size=\"4\" style=\"box-sizing: content-box; position: static; width: 90%; height: 80%;\" id=\"CLOGB3928741016\" data-clog-name=\"project-list\"></select><button style=\"box-sizing: content-box; position: static;\" class=\"w3-margin\" id=\"CLOGB3928741017\" data-clog-name=\"select-button\">Select Project Directory</button></div>"
                            :hidden hidden :class class :html-id html-id
                            :auto-place auto-place)
           'project-dir)))
     (setf (slot-value panel 'select-button)
-            (attach-as-child clog-obj "CLOGB3871639551" :clog-type
+            (attach-as-child clog-obj "CLOGB3928741017" :clog-type
              'clog:clog-button :new-id t))
     (setf (slot-value panel 'project-list)
-            (attach-as-child clog-obj "CLOGB3871639550" :clog-type
+            (attach-as-child clog-obj "CLOGB3928741016" :clog-type
              'clog:clog-select :new-id t))
     (setf (slot-value panel 'frame)
-            (attach-as-child clog-obj "CLOGB3871639549" :clog-type
+            (attach-as-child clog-obj "CLOGB3928741015" :clog-type
              'clog:clog-div :new-id t))
     (let ((target (project-list panel)))
       (declare (ignorable target))
-      (add-select-options (project-list panel)
-       (projects-local-directories))
-      (setf (value (project-list panel))
-              (car (projects-local-directories))))
+      (add-select-options (project-list panel) (projects-local-directories))
+      (setf (value (project-list panel)) (car (projects-local-directories))))
     (clog:set-on-double-click (project-list panel)
                               (lambda (target)
                                 (declare (ignorable target))
